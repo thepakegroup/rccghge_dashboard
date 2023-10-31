57fe0bf69f1be77ffd00889e1af02956f09bbdc4
Prisca Tonia <priscaebube@gmail.com>
2023-10-31 23:45:50 +0100

    API URL Fixes

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,13 +1,13 @@
-export const baseUrl = 'https://api1.kouakoudomagni.com/'
-export const adminBaseUrl = `${baseUrl}admin/`
+export const baseUrl = "https://api.kouakoudomagni.com/";
+export const adminBaseUrl = `${baseUrl}admin/`;
 
 export const labels = [
-    { label: 'Social Media', value: 'SOCIAL_MEDIA' },
-    { label: 'Media', value: 'MEDIA' },
-    { label: 'Social', value: 'SOCIAL' },
-    { label: 'Resources', value: 'RESOURCES' },
-    { label: 'Give', value: 'GIVE' },
-    { label: 'Events', value: 'EVENTS' },
-    { label: 'Activities', value: 'ACTIVITIES' },
-    { label: 'Home', value: 'HOME' },
+  { label: "Social Media", value: "SOCIAL_MEDIA" },
+  { label: "Media", value: "MEDIA" },
+  { label: "Social", value: "SOCIAL" },
+  { label: "Resources", value: "RESOURCES" },
+  { label: "Give", value: "GIVE" },
+  { label: "Events", value: "EVENTS" },
+  { label: "Activities", value: "ACTIVITIES" },
+  { label: "Home", value: "HOME" },
 ];
