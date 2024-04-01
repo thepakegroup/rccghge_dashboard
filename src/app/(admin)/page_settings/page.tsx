69270382e4c71810e21cb0114aa4b97ce904d9b3
Prisca Tonia <priscaebube@gmail.com>
2024-04-02 00:57:36 +0100

    Working on selecting multiple files

---
+++ src/app/(admin)/page_settings/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { MultipleImageUploader } from "@/components/MultipleFilesUploader";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <MultipleImageUploader />
+    </div>
+  );
+};
+
+export default page;
