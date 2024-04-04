e0a549b53f6582df186662248a861114b1ad808d
Prisca Tonia <priscaebube@gmail.com>
2024-04-04 14:22:52 +0100

    Minor fix

--- src/store/slice/mission.ts
+++ src/store/slice/mission.ts
@@ -1,47 +1,43 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-interface serviceI{
+interface serviceI {
   title?: string;
   id?: number | null;
   description?: string;
   category: string;
-  btnType?: 'edit' | 'add';
+  btnType?: "edit" | "add";
 }
 
 const missionSlice = createSlice({
-  name: 'mission',
+  name: "mission",
   initialState: {
     title: "",
-    category: "all",
+    category: "",
     id: null,
-    description:"",
+    description: "",
     btnType: "add",
   } as serviceI,
   reducers: {
-    setMission: (state, action:PayloadAction<serviceI>) => {
-      state.title = action.payload.title
-      state.category = action.payload.category
-      state.description = action.payload.description
-      state.id = action.payload.id
-      state.btnType = action.payload.btnType
+    setMission: (state, action: PayloadAction<serviceI>) => {
+      state.title = action.payload.title;
+      state.category = action.payload.category;
+      state.description = action.payload.description;
+      state.id = action.payload.id;
+      state.btnType = action.payload.btnType;
     },
     setTitle: (state, action) => {
-      state.title = action.payload
+      state.title = action.payload;
     },
     setCategory: (state, action) => {
-      state.category = action.payload
+      state.category = action.payload;
     },
     setDiscription: (state, action) => {
-      state.description = action.payload
+      state.description = action.payload;
     },
   },
 });
 
-export const {
-  setMission,
-  setTitle,
-  setCategory,
-  setDiscription
-} = missionSlice.actions;
+export const { setMission, setTitle, setCategory, setDiscription } =
+  missionSlice.actions;
 
 export default missionSlice.reducer;
